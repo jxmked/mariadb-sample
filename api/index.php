47066720736ee84854405a4b9652ab5700e80ca3
jxmked <jamc015lol@gmail.com>
2022-12-12 00:34:47 +0800

    update

--- api/index.php
+++ api/index.php
@@ -1,62 +1,78 @@
-<?php
+<?php 
+
 header('Access-Control-Allow-Origin: *');
 header("Content-type: application/json; charset=utf-8");
 
+$connection = [
+    "host" => "localhost:3306",
+    "user" => "root",
+    "password" => "123456",
+    "database" => "mariadb_sample",
+    "table" => "fav_cats"
+];
+
+// Initialize connection
+$requests = mysqli_connect($connection['host'], $connection['user'], $connection['password'], $connection['database']);
+
+
+$data = [];
+$query = "";
 
 /**
- * Source: https://stackoverflow.com/questions/4064444/returning-json-from-a-php-script
- * 
- * returnJsonHttpResponse
- * @param $success: Boolean
- * @param $data: Object or Array
- */
-function returnJsonHttpResponse($success, $data) {
-    // remove any string that could create an invalid JSON 
-    // such as PHP Notice, Warning, logs...
-    if (ob_get_contents()) ob_end_clean();
+ * Validate our last-modified input using regex
+ * format: Y-M-D H-M-S
+ * */
+$pattern_last_mod = "/^(\d{4}\-\d{2}\-\d{2})\s(\d{2}:\d{2}:\d{2})$/";
 
-    // this will clean up any previously added headers, to start clean
-   // header_remove(); 
 
-    // Set the content type to JSON and charset 
-    // (charset can be set to something else)
-    //header('Access-Control-Allow-Origin', '*');
-   // header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+if(isset($_POST['last-modified']) && ! empty($_POST['last-modified'])) {
+    $lastModified = $_POST['last-modified'];
     
-    // Set your HTTP response code, 2xx = SUCCESS, 
-    // anything else will be error, refer to HTTP documentation
-    if ($success) {
-        http_response_code(200);
-    } else {
-        http_response_code(500);
+    if(preg_match($pattern_last_mod, $lastModified) == 0) {
+        // Bad Request
+        http_response_code(400);
+        echo "{\"status\":\"Bad Request\",\"body\":\"Invalid post data (last-modified)\"}";
+        exit();
     }
+    /**
+     * Get data if the field last_modified from database is greater
+     * than $lastModified variable
+     * */
+    $ret = "SELECT * from `%s` WHERE last_modified > '%s'";
+    $query = sprintf($ret, $connection['table'], $lastModified);
     
-    // encode your PHP Object or Array into a JSON string.
-    // stdClass or array
-    echo json_encode($data);
-
-    // making sure nothing is added
+}
+/*
+if(empty($query)) {
+    http_response_code(428);
+    
+    echo "{\"status\":\"No Available Data\"}";
+    
     exit();
 }
 
+$result = mysqli_query($requests, $query);
 
-require_once './db.php';
-use DB\DB;;
-
-$var = new DB("localhost:3306", "root", "123456");
-
-$var->open_db("mariadb_sample");
-$req = $var->get_request();
+$data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+*/
 
 
-$query = "SELECT * FROM `fav_cats`";
-$result = mysqli_query($req, $query);
+/**
+ * Response to all requests from different Origin
+ * 
+ * Application JSON Header
+ * */
+header("X-DATA-STATUS: all-latest");
 
-$json = mysqli_fetch_all ($result, MYSQLI_ASSOC);
+// Clean Up - Flush
+if (ob_get_contents()) ob_end_clean();
 
 
+http_response_code(200);
 
-returnJsonHttpResponse(true, $json);
 
+echo preg_match($pattern_last_mod, $_POST["last-modified"]);
+//echo json_encode($data);
 
+exit();
 ?>
