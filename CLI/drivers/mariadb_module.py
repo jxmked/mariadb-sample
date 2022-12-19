0993bf18983dcddfd72d166a338da59ecf4692ac
jxmked <jamc015lol@gmail.com>
2022-12-20 00:22:16 +0800

    update

--- CLI/drivers/mariadb_module.py
+++ CLI/drivers/mariadb_module.py
@@ -3,16 +3,18 @@
 
 
 from os import getenv as env
+from sys import exit
 import mariadb
 
 
 class MariaDB:
-    # Secret variables
+    # Secrets
     __sk_host__ = env("mariadb_host")
     __sk_port__ = env("mariadb_port")
     __sk_user__ = env("mariadb_user")
     __sk_pwd__ = env("mariadb_pwd")
     __sk_db__ = env("mariadb_db")
+    __open_db__ = {}
     
     def __init__(self):
         print(MariaDB.__sk_db__)
@@ -26,13 +28,14 @@ class MariaDB:
                 password=MariaDB.__sk_pwd__,
                 host=MariaDB.__sk_host__,
                 port=MariaDB.__sk_port__,
-                database=MariaDB.__sk_db__
+                database=db
             )
+            conn.auto_reconnect = True
         except mariadb.Error as e:
-            print(f"Error connecting to MariaDB Platform: {e}")
-            sys.exit(1)
-        
-        return None
+            print("Opening Database: Failed")
+            print(f"User: {MariaDB.__sk_user__}")
+            print(f"Database: {db}")
+            exit(1)
         
     def check(self):
         # Return 0 if passed
