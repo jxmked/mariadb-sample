213a7c63cd281be9aec1cbf42d618c21173f8474
jxmked <jamc015lol@gmail.com>
2022-12-24 03:15:32 +0800

    update

--- CLI/ui/modify.py
+++ CLI/ui/modify.py
@@ -5,7 +5,7 @@ from utils.console_in import ConsoleIn
 from ui.empty_rows import EmptyRows
 from ui.insert_form import InsertForm
 from ui.view_data import ViewData
-
+ 
 class UIModify:
 
     def __init__(self):
